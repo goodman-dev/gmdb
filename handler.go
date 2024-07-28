ebb0cb172eada937bd64cc379b151ee486804e61
GoodmanDev <david.g.goodman@bath.edu>
2024-07-28 23:01:57 +0100

    Add hash commands

--- handler.go
+++ handler.go
@@ -6,9 +6,12 @@ import (
 )
 
 var Handlers = map[string]func([]Value) Value{
-	"PING": ping,
-	"SET":  set,
-	"GET":  get,
+	"PING":    ping,
+	"SET":     set,
+	"GET":     get,
+	"HSET":    hset,
+	"HGET":    hget,
+	"HGETALL": hgetall,
 }
 
 func ping(args []Value) Value {
@@ -57,5 +60,77 @@ func get(args []Value) Value {
 	}
 
 	return BulkValue{bulk: value}
+
+}
+
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
+
+func hset(args []Value) Value {
+
+	if len(args) != 3 {
+		return ErrorValue{err: fmt.Errorf("ERR wrong number of arguments for the 'hset' command")}
+	}
+
+	hash := args[0].(BulkValue).bulk
+	key := args[1].(BulkValue).bulk
+	value := args[2].(BulkValue).bulk
+
+	HSETsMu.Lock()
+
+	if _, ok := HSETs[hash]; !ok {
+		HSETs[hash] = map[string]string{}
+	}
+	HSETs[hash][key] = value
+	HSETsMu.Unlock()
+
+	return StringValue{str: "OK"}
+
+}
+
+func hget(args []Value) Value {
+
+	if len(args) != 2 {
+		return ErrorValue{err: fmt.Errorf("ERR wrong number of arguments for the 'hget' command")}
+	}
+
+	hash := args[0].(BulkValue).bulk
+	key := args[1].(BulkValue).bulk
+
+	HSETsMu.RLock()
+	value, ok := HSETs[hash][key]
+	HSETsMu.RUnlock()
+
+	if !ok {
+		return NullValue{}
+	}
+
+	return BulkValue{bulk: value}
+
+}
+
+func hgetall(args []Value) Value {
+
+	if len(args) != 1 {
+		return ErrorValue{err: fmt.Errorf("ERR wrong number of arguments for the 'hgetall' command")}
+	}
+
+	hash := args[0].(BulkValue).bulk
+
+	HSETsMu.RLock()
+	value, ok := HSETs[hash]
+	HSETsMu.RUnlock()
+
+	if !ok {
+		return NullValue{}
+	}
+
+	values := []Value{}
+	for k, v := range value {
+		values = append(values, BulkValue{bulk: k})
+		values = append(values, BulkValue{bulk: v})
+	}
+
+	return ArrayValue{array: values}
 
 }
